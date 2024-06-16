a8593b0e0162ea628dc424b69000ea0f7bbf7452
QIXIUQX <qingwu13950520@outlook.com>
2024-06-16 23:29:36 +0800

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="" id="app">
+    <router-view />
+  </div>
+</template>
+
+<script>
+/** 组件/工具类/store 导入 */
+
+/** 网络请求导入 导入 */
+
+export default {
+  name: "App",
+  methods: {
+    /** 事件或其他相关方法 **/
+    name() {
+
+    }
+    /** 网络请求相关方法 **/
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+#app{
+  padding: 10px 20px;
+}
+</style>
---
+++ src/views/Convert/Convert.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="">123</div>
+</template>
+
+<script>
+/** 组件/工具类/store 导入 */
+
+/** 网络请求导入 导入 */
+
+export default {
+	name: "Convert",
+	methods: {
+		/** 事件或其他相关方法 **/
+		name() {
+
+		}
+		/** 网络请求相关方法 **/
+	},
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
